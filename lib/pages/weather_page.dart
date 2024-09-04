9e3a2327c35a29566a7bf78718c488c75ad2eec1
Ana Buhaescu <buhaescuana@yahoo.com>
2024-09-04 15:00:55 +0300

    building weather app

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_weather_app/models/weather_model.dart';
+import 'package:minimal_weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('250f57fa0ea594a62c37add5ca1d638b');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Loading city..'),
+            Text('${_weather?.temperature.round()}C')
+          ],
+        ),
+      ),
+    );
+  }
+}

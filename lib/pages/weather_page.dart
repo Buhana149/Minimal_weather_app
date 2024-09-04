49e8e84cafda3c410a8c17efdb852e3ef2aaa00d
Ana Buhaescu <buhaescuana@yahoo.com>
2024-09-04 15:58:17 +0300

    ui and errors fixed

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:minimal_weather_app/models/weather_model.dart';
 import 'package:minimal_weather_app/services/weather_service.dart';
 
@@ -26,6 +27,30 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets1/sun.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets1/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets1/rain.jon';
+      case 'thunderstorm':
+        return 'assets1/thunder.json';
+      case 'clear':
+        return 'assets1/sun.json';
+      default:
+        return 'assets1/sun.json';
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -37,12 +62,32 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[100],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_weather?.cityName ?? 'Loading city..'),
-            Text('${_weather?.temperature.round()}C')
+            Icon(
+              Icons.location_on,
+              size: 40,
+              color: Colors.grey[500],
+            ),
+            Text(
+              _weather?.cityName ?? 'Loading city..',
+              style: TextStyle(fontSize: 30, color: Colors.grey[500]),
+            ),
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+            Text(
+              '${_weather?.temperature.round()}°C',
+              style: TextStyle(fontSize: 30, color: Colors.grey[500]),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              _weather?.mainCondition ?? '',
+              style: TextStyle(fontSize: 30, color: Colors.grey[500]),
+            ),
           ],
         ),
       ),
